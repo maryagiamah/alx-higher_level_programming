c757db60c1306bcfe87d24258ad8db418e6c1759
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-20 18:04:12 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return { key: item ** 2 for key, item in a_dictionary.items()}
+    return {key: item ** 2 for key, item in a_dictionary.items()}

b7a196c996763bf6fd41d5daed24098ed6f77aff
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-21 14:22:57 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -41,8 +41,6 @@ class Square:
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value

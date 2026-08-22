bf15ef95810c3d30a2307aa2ff734ae1c2ba809e
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-22 13:12:12 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square():
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value == 0:
+        if value < 0:
            raise ValueError("size must be >= 0")
         self.__size = value
 

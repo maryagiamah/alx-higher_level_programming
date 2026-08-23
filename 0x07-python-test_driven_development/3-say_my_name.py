b9bbe99aa821318f66febbca786e95ab79b9db43
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-23 15:27:47 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,8 +7,8 @@ The 3-say_my_name  module supplies one function, say_my_name.
 
 def say_my_name(first_name, last_name=""):
     """Prints "My name is" followed by the first name and optional last name"""
-    if type(first_name) is not str:
+    if isinstance (first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is", first_name, last_name)

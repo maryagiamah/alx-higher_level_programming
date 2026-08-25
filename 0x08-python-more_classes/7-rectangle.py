3823095349db0ed3dbc2d1bf157eceec04d3b308
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-25 09:38:29 +0100

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -63,7 +63,8 @@ class Rectangle:
         """returns printable string representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.__width for _ in range(self.__height))
+        return "\n".join(str(self.print_symbol) * self.__width
+            for _ in range(self.__height))
 
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""

9749781df37339c7787f5acd0e491f38f0cfd1cf
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-25 10:00:34 +0100

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
-        def __str__(self):
+    def __str__(self):
         """returns printable string representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""

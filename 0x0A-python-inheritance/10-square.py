6b04326b1394c7c90afa622a7b348002aca73552
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-26 21:43:38 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,17 +4,17 @@ Rectangle = __import__('8-rectangle').Rectangle
 
 
 class Square(Rectangle):
-     """ Inherits Rectangle """
+    """ Inherits Rectangle """
      
-     def __init__(self, size):
-          """ size vslidator """
-         self.integer_validator('size', size)
-         self.__size = size
+    def __init__(self, size):
+        """ size vslidator """
+        self.integer_validator('size', size)
+        self.__size = size
               
     def area(self):
         """ Area implementation """
          return self.__size ** 2
 
-     def __str__(self):
+    def __str__(self):
          """ str implementation """
          return "[Rectangle] {}/{}".format(self.__size, self.__size)

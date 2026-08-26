6a9e444f602af6eebfc210c120b7d82b4b81ad26
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-26 23:53:58 +0100

    Create 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Recover object from file"""
+import json
+
+
+def load_from_json_file(filename)
+    """DeSerialisation"""
+
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)

5a6b73cbdafecd647e4e35fc1cd4d69b446050e8
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-26 23:40:34 +0100

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """Serialisation to file"""
 
     with open(filename, encoding="utf-8") as f:
-        f.write(json.dump(my_obj))
+        json.dump(my_obj, f)

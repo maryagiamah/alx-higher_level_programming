826c3062250121839a38f7fe6be5e10874f1ba9f
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-27 08:18:00 +0100

    Create 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from Pathlib import Path
+import sys
+import json
+
+
+def add_item():
+    """Add item to file"""
+
+    objs = sys.argv[1:]
+    filename = sys.argv[0]
+    try:
+        ob = load_from_json_file(filename)
+    except:
+        Path.(filename).touch()
+
+    
+     save_to_json_file(objs, filename)
+  

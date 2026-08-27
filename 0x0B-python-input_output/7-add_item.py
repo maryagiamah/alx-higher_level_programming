c34e071660a98d7638c85cb10b759fa089c69ec7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-27 12:46:04 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """Add obj items to file"""
 import sys
-import os
-from pathlib import Path
-
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
 
 objs = sys.argv[1:]
 filename = "add_item.json"

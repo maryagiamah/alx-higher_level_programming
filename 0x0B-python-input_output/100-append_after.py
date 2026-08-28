a69bccf88dc9468b83c2bcbedc49b0929832decc
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-28 23:09:41 +0100

    Create 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Append after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append after"""
+
+    update_lines = []
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            update_lines.append(line)
+            if search_string in line:
+                 update_lines.append(new_string)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(update_lines)
+

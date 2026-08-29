4857174b17bfe57665d49ad5c4978ea6aa2be1b5
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-29 02:42:43 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """ Init method """
-        super().__init__(self, size, x=0, y=0, id)
+        super().__init__(size, size, x, y, id)
         
     def __str__(self):
         """ str method """

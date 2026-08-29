f629c3001f0716efc6eda253a0236862c75a8de6
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-29 19:51:13 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @static_method
     def to_json_string(list_dictionaries):
         """JSON String"""
         if not list_dictionaries:

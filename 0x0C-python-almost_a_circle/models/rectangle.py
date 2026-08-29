680d6f6e3ce6f1d508ca3892289d04ae340e34d2
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-29 03:16:39 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,7 +19,13 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """To_dict"""
-        return self.__dict__
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
 
     def display(self):
         """Display rectangle method"""

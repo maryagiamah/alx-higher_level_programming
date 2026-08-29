808ff32aa5aca5973cf582b3f121b0c6ab1e4178
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2026-08-29 02:45:36 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Init method """
         super().__init__(size, size, x, y, id)
-        
+
     def __str__(self):
         """ str method """
         return "[Square] ({}) {}/{} - {}".format(
